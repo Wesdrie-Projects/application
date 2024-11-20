93d93ebfcb57d7f750c51db009461654f840157e
Wesdrie.Projects <wesdrie.projects@gmail.com>
2024-11-20 19:19:16 +0200

    Update to use Firebase.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAx-Y4HPzypkrwyvRPFM-idHcPoOVUXxtA",
+  authDomain: "application-eca55.firebaseapp.com",
+  projectId: "application-eca55",
+  storageBucket: "application-eca55.firebasestorage.app",
+  messagingSenderId: "264390168167",
+  appId: "1:264390168167:web:cfae5b9b203f54eb9d4ec2"
+};
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideRouter(routes)
+  ]
 };
